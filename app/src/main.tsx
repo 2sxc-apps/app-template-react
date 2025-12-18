1b20bc1c58d9a2910df2b99131991ffc0e9d6eef
Roman Opalko <roman.opalko@2sic.com>
2025-12-18 09:55:36 +0100

    minors

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,21 +3,23 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// Export a function to bootstrap the app for a module instance
-export function initReact({ moduleId }: { moduleId: number }) {
+const winAny = window as any;
+winAny.reactApp ??= {};
+winAny.reactApp.init ??= initReact;
+
+// function to bootstrap the app for a module instance
+function initReact({ moduleId }: { moduleId: number }) {
   console.log(`React Initiated: root-${moduleId}`)
   const rootEl = document.getElementById(`root-${moduleId}`);
+  
   if (!rootEl) {
     console.error(`No root element found for moduleId ${moduleId}`);
     return;
   }
+
   createRoot(rootEl).render(
     <StrictMode>
       <App moduleId={moduleId} />
     </StrictMode>
   );
 }
-
-const winAny = window as any;
-winAny.reactApp ??= {};
-winAny.reactApp.init ??= initReact;
